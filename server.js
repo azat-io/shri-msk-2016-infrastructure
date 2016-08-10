cac6705114d55ca262095396fc7ce951c0973823
Azat S <to@azat.io>
2016-08-10 22:54:30 +0300

    PostCSS support

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+'use strict'
+
 import express from 'express'
 import path from 'path'
 import chalk from 'chalk'
+
+import postcss from 'postcss-middleware'
+import cssNext from 'postcss-cssnext'
+import atImport from 'postcss-import'
+import csso from 'postcss-csso'
+import responsiveType from 'postcss-responsive-type'
 
 const app = express()
 const port = 3000
@@ -10,16 +18,29 @@ app.set('port', process.env.PORT || port)
 app.set('views', path.join('view'))
 app.set('view engine', 'pug')
 
-app.use(express.static(path.join('public')))
+app.use('/css', postcss({
+  src: (req) => {
+    return path.join(__dirname, 'public', 'css', req.path)
+  },
+  plugins: [
+    atImport,
+    responsiveType,
+    cssNext,
+    csso
+  ]
+}))
 
-app.get('/', (request, response) => {
+app.use(
+  express.static(path.join('public'))
+)
+
+app.get('/', (req, res) => {
   console.time('render')
-  response.render('index')
+  res.render('index')
   console.timeEnd('render')
 })
 
 app.listen(app.get('port'), () => {
-  console.log(chalk.white.bold('Server has started at port: ') +
+  console.log(chalk.bold('Server has started at port: ') +
     chalk.green.bold(app.get('port')))
 })
-
