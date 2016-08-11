dd8c21be76eaf691662ac958a9ab61aade82ac91
Azat S <to@azat.io>
2016-08-11 12:12:23 +0300

    Update deprecated assertions

--- test.js
+++ test.js
@@ -2,17 +2,17 @@ import test from 'ava'
 import helpers from './helpers'
 
 test('Вычисление площади квадрата', t => {
-  t.same(helpers.squareArea(2), 4)
+  t.deepEqual(helpers.squareArea(2), 4)
 })
 
 test('Вычисление площади прямоугольника', t => {
-  t.same(helpers.rectangeArea(2, 3), 6)
+  t.deepEqual(helpers.rectangeArea(2, 3), 6)
 })
 
 test('Вычисление площади треугольника', t => {
-  t.same(helpers.triangleArea(3, 4, 5), 6)
+  t.deepEqual(helpers.triangleArea(3, 4, 5), 6)
 })
 
 test('Вычисление площади круга', t => {
-  t.same(helpers.circleArea(10), 31.42)
+  t.deepEqual(helpers.circleArea(10), 31.42)
 })
